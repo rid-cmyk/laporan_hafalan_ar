57bf3c6587431cc14cdd2297bdf89349c40274cc
Hendri Sudianto <hendri@go-jek.com>
2025-09-15 20:07:30 +0700

    update database used prisma clouds

--- app/api/roles/route.ts
+++ app/api/roles/route.ts
@@ -1,7 +1,8 @@
-import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+import prisma from "@/lib/prisma"; 
+import { NextResponse } from "next/server";
+export const runtime = 'edge';
+
 
 // GET all roles
 export async function GET() {
